0d9dcb8c3d74e2e770e318e0c6c2b2edc6a41ea1
Deyvidjh30 <c.deyvid@escolar.ifrn.edu.br>
2025-09-25 14:45:59 +0000

    criei viewa e horario novamente

--- views.py
+++ views.py
@@ -1,10 +1,11 @@
 from models.cliente import Cliente, ClienteDAO
 from models.servico import Servico, servicoDAO
-
+from models.horario import Horario, HorarioDAO
 
 
 class View:
 
+    # Métodos para Cliente
     @staticmethod
     def cliente_listar():
         return ClienteDAO.listar()
@@ -24,7 +25,7 @@ class View:
         cliente = Cliente(id, "", "", "")
         ClienteDAO.excluir(cliente)
 
-
+    # Métodos para Serviço
     @staticmethod
     def servico_listar():
         return servicoDAO.listar()
@@ -43,3 +44,29 @@ class View:
     def servico_excluir(id):
         servico = Servico(id, "", 0)
         servicoDAO.excluir(servico)
+
+    # Métodos para Horário
+    @staticmethod
+    def horario_inserir(data, confirmado, id_cliente, id_servico):
+        c = Horario(0, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.inserir(c)
+
+    @staticmethod
+    def horario_listar():
+        return HorarioDAO.listar()
+
+    @staticmethod
+    def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
+        c = Horario(id, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.atualizar(c)
+
+    @staticmethod
+    def horario_excluir(id):
+        c = Horario(id, None)
+        HorarioDAO.excluir(c)
