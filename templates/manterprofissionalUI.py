0ba33db0f43aa3f5ee023d2aafea5412ed0e92f7
Davi Miranda <davi.miranda@escolar.ifrn.edu.br>
2025-10-09 14:17:42 +0000

    ..

--- templates/manterprofissionalUI.py
+++ templates/manterprofissionalUI.py
@@ -30,11 +30,12 @@ class ManterProfissionalUI:
     def inserir():
         nome = st.text_input("Informe o nome")
         email = st.text_input("Informe o e-mail")
-        fone = st.text_input("Informe o fone")
+        especialidade = st.text_input("Informe a especialidade")
+        conselho = st.text_input("Informe a conselho")
         senha = st.text_input("Informe a senha", type="password")
 
         if st.button("Inserir"):
-            View.profissional_inserir(nome, email, fone, senha)
+            View.profissional_inserir(nome, email, especialidade, senha,conselho)
             st.success("Profissional inserido com sucesso")
             time.sleep(1)
             st.rerun()
@@ -49,11 +50,12 @@ class ManterProfissionalUI:
         op = st.selectbox("Selecione um profissional", profissionais, format_func=lambda p: p.get_nome())
         nome = st.text_input("Novo nome", op.get_nome())
         email = st.text_input("Novo e-mail", op.get_email())
-        fone = st.text_input("Novo fone", op.get_fone())
+        especialidade = st.text_input("Nova especialidade", op.get_especialidade())
+        conselho = st.text_input("Novo conselho", op.get_conselho())
         senha = st.text_input("Nova senha", op.get_senha(), type="password")
 
         if st.button("Atualizar"):
-            View.profissional_atualizar(op.get_id(), nome, email, fone, senha)
+            View.profissional_atualizar(op.get_id(), nome, email, especialidade, senha,conselho)
             st.success("Profissional atualizado com sucesso")
 
     @staticmethod
